f816d64553e01770e987378a7146155f8ab9289e
sharvandeep <2400032427@kluniversity.in>
2025-10-22 06:05:02 -0400

    Final project completion: Redux integration and build

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 // src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import ProductList from "./components/ProductList";
-import CartPage from "./components/CartPage";
+import Header from "./components/Header.jsx";
+import ProductList from "./components/ProductList.jsx";
+import CartPage from "./components/CartPage.jsx";
 
 export default function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,7 @@ import store from "./store.js";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
